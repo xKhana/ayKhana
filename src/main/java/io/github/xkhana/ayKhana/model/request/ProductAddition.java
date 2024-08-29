5230f3a794bd56fff1c3a6947abfeb0126cdeefb
Mohamed Sallam <59128458+mohamed-sallam@users.noreply.github.com>
2024-08-29 17:50:01 +0300

    config/config/#1/add_spring_basic_config: Create boilerplate structure

---
+++ src/main/java/io/github/xkhana/ayKhana/model/request/ProductAddition.java
@@ -0,0 +1,4 @@
+package io.github.xkhana.ayKhana.model.request;
+
+public class ProductAddition {
+}
---
+++ src/main/java/io/github/xkhana/ayKhana/service/ProductRepository.java
@@ -0,0 +1,4 @@
+package io.github.xkhana.ayKhana.service;
+
+public interface ProductRepository {
+}
